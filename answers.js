3091d3d88c7241697cda3067e71b7dff81aa1469
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-10 19:59:37 +0000

    exercise 1

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
 // one line comment
+
+/*
+Write a function that takes a string and returns the first character of the string. 
+Test your function on a few inputs, including the empty string.
+*/
+
+function firstCharacter(str) {
+    return str.charAt(0);
+}
+
+console.log(firstCharacter("Ananadad"));
