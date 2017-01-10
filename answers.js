02bdd2d6d36d6e1716c6704903ba24bf535977d9
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-10 20:11:50 +0000

    2nd exercise

--- answers.js
+++ answers.js
@@ -10,3 +10,15 @@ function firstCharacter(str) {
 }
 
 console.log(firstCharacter("Ananadad"));
+
+/*Write a function that takes a string and returns the last 
+character of a string. Test your function on a few inputs, 
+including the empty string. */
+
+function lastCharacter(str) {
+    return str.slice(-1);
+}
+console.log(lastCharacter("Ani"));
+
+
+
